4cc25b3c926b60adc921ad3a27173f1560ce719d
Ethanbst <ethan.basti1@gmail.com>
2025-03-09 16:27:18 +0100

    - Mise à jour du numéro de version du jeu

--- menu/menu.c
+++ menu/menu.c
@@ -59,7 +59,7 @@ void menu_start(SDL_Renderer *renderer, SDL_Surface *bg_menu_surface, SDL_Textur
 
     color couleur = {255, 255, 255, 255}; //Couleur blanche
 
-    CTA version = draw_button(renderer, ecran.w-200, ecran.h-120, 1, "Alpha 0.38.11", 1, 48, couleur);
+    CTA version = draw_button(renderer, ecran.w-200, ecran.h-120, 1, "Alpha 0.44.11", 1, 48, couleur);
     draw_button(renderer, ecran.w-200, version.pos_y+version.h+20, 1, "By E. Bastien", 1, 48, couleur);
 
 
