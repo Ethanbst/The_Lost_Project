8c368231e7215cca239d7b69da59404e019480d8
Ethanbst <ethan.basti1@gmail.com>
2025-03-16 14:16:20 +0100

    - Suppression d'une code inutilisé

--- logs_utils/log.c
+++ logs_utils/log.c
@@ -23,56 +23,6 @@ void create_log_file() {
         perror("Error creating log file");
     }
 }
-/*
-void add_log2(const char *texte) {
-    // Récupère la pile d'appels
-    #ifdef _WIN32
-    void *buffer[10];
-    USHORT frames = CaptureStackBackTrace(0, 10, buffer, NULL);
-
-    // Initialiser le débogueur
-    HANDLE process = GetCurrentProcess();
-    SymInitialize(process, NULL, TRUE);
-
-    // Allouer de la mémoire pour les symboles
-    SYMBOL_INFO *symbol = (SYMBOL_INFO *)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);
-    symbol->MaxNameLen = 255;
-    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-
-    // Obtenir les symboles pour chaque frame
-    for (USHORT i = 0; i < frames; i++) {
-        SymFromAddr(process, (DWORD64)(buffer[i]), 0, symbol);
-        printf("%i: %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address);
-    }
-
-    // Libérer la mémoire allouée pour les symboles
-    free(symbol);
-    SymCleanup(process);
-
-    const char *origine = (frames > 1) ? "Captured Stack Trace" : "Unknown";
-    #else
-    void *buffer[10];
-    char **callstack;
-    int frames = backtrace(buffer, 10);
-    callstack = backtrace_symbols(buffer, frames);
-    const char *origine = (frames > 1) ? callstack[1] : "Unknown";
-    #endif
-
-    FILE *file = fopen(last_logfile, "a");
-    if (file != NULL) {
-        printf("%s: %s", origine, texte);
-        fprintf(file, "%s: %s", origine, texte);
-        fclose(file);
-    } 
-    else {
-        perror("Error opening last_logfile");
-    }
-
-    #ifndef _WIN32
-    free(callstack);
-    #endif
-}
-*/
 
 //Ajoute la chaine au journal d'évènements et l'affiche également dans la console
 void add_log(const char *origine, const char *texte) {
